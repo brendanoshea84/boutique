98cd7f377b68f1d76625f82ad3fe53531ff1651b
brendanoshea84 <b_oshea_84@hotmail.com>
2020-06-24 13:01:06 +0200

    finished coding

--- bag/contexts.py
+++ bag/contexts.py
@@ -20,7 +20,7 @@ def bag_contents(request):
                 'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
-        })
+                })
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
@@ -31,18 +31,18 @@ def bag_contents(request):
                     'quantity': quantity,
                     'product': product,
                     'size': size,
-        })
+                    })
 
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
+    if total<settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total 
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
 
     else:
         delivery = 0
         free_delivery_delta = 0
 
-    grand_total = delivery + total   
+    grand_total = delivery + total
 
     context = {
         'bag_items': bag_items,
@@ -52,7 +52,6 @@ def bag_contents(request):
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
-        
     }
 
     return context
